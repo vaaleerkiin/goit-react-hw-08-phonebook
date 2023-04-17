57750132aa6f6c9e878a80c49e03ca05634e3224
Valera Kamelkov <tahaburrent@gmail.com>
2023-04-17 11:46:19 +0200

    ~

--- src/components/Phonebook/Phonebook.jsx
+++ src/components/Phonebook/Phonebook.jsx
@@ -8,10 +8,10 @@ import { nanoid } from 'nanoid';
 export class Phonebook extends Component {
   state = {
     contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      { id: nanoid(10), name: 'Rosie Simpson', number: '459-12-56' },
+      { id: nanoid(10), name: 'Hermione Kline', number: '443-89-12' },
+      { id: nanoid(10), name: 'Eden Clements', number: '645-17-79' },
+      { id: nanoid(10), name: 'Annie Copeland', number: '227-91-26' },
     ],
     filter: '',
   };
@@ -35,7 +35,7 @@ export class Phonebook extends Component {
         return {
           contacts: [
             ...prevState.contacts,
-            { id: nanoid(4), name, number: this.formatNumber(number) },
+            { id: nanoid(10), name, number: this.formatNumber(number) },
           ],
         };
     });
@@ -46,6 +46,7 @@ export class Phonebook extends Component {
   };
 
   visibleContacts = () => {
+    console.log(this.state.contacts);
     return [...this.state.contacts].filter(({ name }) =>
       name.toLowerCase().includes(this.state.filter.toLowerCase())
     );
