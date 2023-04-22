2d0898df7a047a5486b196b3eb36e7a8f8c50bb0
Valera Kamelkov <tahaburrent@gmail.com>
2023-04-22 23:42:40 +0200

    finish

--- src/components/Phonebook/Phonebook.jsx
+++ src/components/Phonebook/Phonebook.jsx
@@ -3,17 +3,30 @@ import { PhonebookForm } from 'components/Phonebook/PhonebookForm';
 import { PhonebookList } from 'components/Phonebook/PhonebookList';
 import { PhonebookFilter } from 'components/Phonebook/PhonebookFilter';
 import { PhonebookWrap } from 'components/Phonebook/Phonebook.styled';
-
 import { nanoid } from 'nanoid';
+const LOCAL_KEY = 'contacts';
 export class Phonebook extends Component {
   state = {
     contacts: [
-      { id: nanoid(10), name: 'Rosie Simpson', number: '459-12-56' },
-      { id: nanoid(10), name: 'Hermione Kline', number: '443-89-12' },
-      { id: nanoid(10), name: 'Eden Clements', number: '645-17-79' },
-      { id: nanoid(10), name: 'Annie Copeland', number: '227-91-26' },
+      // { id: nanoid(10), name: 'Rosie Simpson', number: '459-12-56' },
+      // { id: nanoid(10), name: 'Hermione Kline', number: '443-89-12' },
+      // { id: nanoid(10), name: 'Eden Clements', number: '645-17-79' },
+      // { id: nanoid(10), name: 'Annie Copeland', number: '227-91-26' },
     ],
     filter: '',
+  };
+
+  componentDidMount = () => {
+    if (localStorage.getItem(LOCAL_KEY)) {
+      const contacts = JSON.parse(localStorage.getItem(LOCAL_KEY));
+      this.setState({ contacts });
+    }
+  };
+
+  componentDidUpdate = (prevProps, prevState) => {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem(LOCAL_KEY, JSON.stringify(this.state.contacts));
+    }
   };
 
   formatNumber = number => {
