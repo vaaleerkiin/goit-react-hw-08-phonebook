ddf1b6aa61331168397ed228c63eb64029be7e3b
Valera Kamelkov <tahaburrent@gmail.com>
2023-06-11 20:36:40 +0200

    Update PhonebookList.jsx

--- src/components/Phonebook/PhonebookList.jsx
+++ src/components/Phonebook/PhonebookList.jsx
@@ -1,6 +1,5 @@
 import { useSelector } from 'react-redux';
 import { toast } from 'react-toastify';
-
 import BeatLoader from 'react-spinners/BeatLoader';
 import { PhonebookItem } from './PhonebookItem';
 
