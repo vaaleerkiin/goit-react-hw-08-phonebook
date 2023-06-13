98da30f6ed9d95cb43bad34e24f42caf95a5fc59
Valera Kamelkov <tahaburrent@gmail.com>
2023-06-13 16:34:27 +0200

    ~

--- src/components/Phonebook/PhonebookFilter.jsx
+++ src/components/Phonebook/PhonebookFilter.jsx
@@ -1,26 +1,18 @@
 import { useSelector, useDispatch } from 'react-redux';
 import { changeFilter } from 'redux/filterSlice';
-import { Input, Tooltip } from 'antd';
+import { Input } from 'antd';
 
 export const PhonebookFilter = () => {
   const state = useSelector(state => state.filter);
   const dispatch = useDispatch();
   return (
-    <div>
-      <Tooltip
-        trigger={['focus']}
-        title="Find contacts by name"
-        placement="topLeft"
-      >
-        <Input
-          type="text"
-          name="filter"
-          placeholder="Find contacts by name"
-          value={state}
-          style={{ maxWidth: 300 }}
-          onChange={e => dispatch(changeFilter({ value: e.target.value }))}
-        />
-      </Tooltip>
-    </div>
+    <Input
+      type="text"
+      name="filter"
+      placeholder="Find contacts by name"
+      value={state}
+      style={{ maxWidth: 200 }}
+      onChange={e => dispatch(changeFilter({ value: e.target.value }))}
+    />
   );
 };
--- src/index.js
+++ src/index.js
@@ -10,15 +10,15 @@ import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  //<React.StrictMode>
-  <ThemeProvider theme={theme}>
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/goit-react-hw-08-phonebook">
-          <App />
-        </BrowserRouter>
-      </PersistGate>
-    </Provider>
-  </ThemeProvider>
-  //</React.StrictMode>
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <BrowserRouter basename="/goit-react-hw-08-phonebook">
+            <App />
+          </BrowserRouter>
+        </PersistGate>
+      </Provider>
+    </ThemeProvider>
+  </React.StrictMode>
 );
