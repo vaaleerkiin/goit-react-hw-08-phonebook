31352b5ab828adb75f24fe147a02abfbac98dffb
Valera Kamelkov <tahaburrent@gmail.com>
2023-08-05 14:07:09 +0200

    Update SharedLayout.tsx

--- src/components/SharedLayout/SharedLayout.tsx
+++ src/components/SharedLayout/SharedLayout.tsx
@@ -21,7 +21,7 @@ import { useLogoutMutation } from "redux/Auth/operations";
 import MoonLoader from "react-spinners/MoonLoader";
 import { RootState } from "redux/store";
 import { PhonebookAvatar } from "components/Phonebook/PhonebookAvatar";
-const defailtAvatar =
+const defaultAvatar =
   "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/271deea8-e28c-41a3-aaf5-2913f5f48be6/de7834s-6515bd40-8b2c-4dc6-a843-5ac1a95a8b55.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzI3MWRlZWE4LWUyOGMtNDFhMy1hYWY1LTI5MTNmNWY0OGJlNlwvZGU3ODM0cy02NTE1YmQ0MC04YjJjLTRkYzYtYTg0My01YWMxYTk1YThiNTUuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.BopkDn1ptIwbmcKHdAOlYHyAOOACXW0Zfgbs0-6BY-E";
 interface IProps {
   refetch: () => void;
@@ -65,8 +65,8 @@ export const SharedLayout: React.FC<IProps> = ({ refetch }) => {
                 <ButtonAvatar onClick={toogleModal}>
                   <UserAvatar
                     src={
-                      avatarURL === defailtAvatar
-                        ? defailtAvatar
+                      avatarURL === defaultAvatar
+                        ? defaultAvatar
                         : `https://phonebook-0e5s.onrender.com/${avatarURL}`
                     }
                   />
